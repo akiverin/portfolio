2f0a5f633274135a39df6f737f1eda782e4b5a25
“akiverin” <kiverin03@yandex.ru>
2023-08-22 22:15:04 +0300

    add bg

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="window" :style="{ transform: `translateX(${rotation}%) translateY(${rotation*2}%) translateZ(0px)` }"></div>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -8,19 +9,49 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      rotation: 0,
+    };
+  },
+  mounted() {
+    var self = this;
+    setInterval(function () {
+      self.rotation = Math.floor(Math.random()*10);
+    }, 100);
+  },
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+  background-color: rgb(26, 24, 30);
+  padding: 0;
+  margin: 0;
+}
+.window {
+  background: url("https://framerusercontent.com/images/rR6HYXBrMmX4cRpXfXUOvpvpB0.png");
+  opacity: 0.1;
+  inset: -200%;
+  width: 400%;
+  height: 400%;
+  position: absolute;
+  z-index: -1;
+}
+
 #app {
+  position: relative;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
 }
 </style>
